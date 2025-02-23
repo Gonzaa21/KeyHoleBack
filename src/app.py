6ba11c173ff4c460950be4f6625c523a01624210
Gonzaa21 <gonzidibujante@gmail.com>
2025-02-23 11:24:47 -0300

    __init__ package and fixing fetch conection

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     
-    app.config.from_object("app.config")
+    app.config.from_object("src.config")
 
     init_db(app)
 
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
     return jsonify({"mensaje": "Backend funcionando correctamente"})
+
+# Verifying fetch conection
+
+@main.route('/dashboard/objectives', methods=['POST'])
+def get_data():
+    data = request.get_json()
+    print("Datos recibidos:", data)
+    return jsonify({"message": "Datos recibidos"}), 200
